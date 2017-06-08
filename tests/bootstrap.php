a0c976a020ebdeea87ddd6bbab3c2114e393d57a
Reinder Reinders <reinder@level23.nl>
2017-06-08 15:28:48 +0200

    Updated test to use Bits constants

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,21 +6,21 @@
 
 namespace Reinder83\BinaryFlags\Tests {
 
-    require __DIR__ .'/../vendor/autoload.php';
+    require __DIR__ . '/../vendor/autoload.php';
 
     use Reinder83\BinaryFlags\BinaryFlags;
+    use Reinder83\BinaryFlags\Bits;
 
     /**
      * Simple Example class that only defines constants
      *
      * @package Reinder83\BinaryFlags
      */
-    class ExampleFlags extends BinaryFlags {
-
-        const FOO = 1;
-        const BAR = 2;
-        const BAZ = 4;
-
+    class ExampleFlags extends BinaryFlags
+    {
+        const FOO = Bits::BIT_1;
+        const BAR = Bits::BIT_2;
+        const BAZ = Bits::BIT_3;
     }
 
     /**
@@ -28,11 +28,11 @@ namespace Reinder83\BinaryFlags\Tests {
      *
      * @package Reinder83\BinaryFlags
      */
-    class ExampleFlagsWithNames extends BinaryFlags  {
-
-        const FOO = 1;
-        const BAR = 2;
-        const BAZ = 4;
+    class ExampleFlagsWithNames extends BinaryFlags
+    {
+        const FOO = Bits::BIT_1;
+        const BAR = Bits::BIT_2;
+        const BAZ = Bits::BIT_3;
 
         public static function getAllFlags()
         {
